b8c90d0567f060b52fcc55e38304dbcd88861f15
ACER <ralphkaiserdaclan>
2022-11-19 22:56:07 +0800

    Mathematical Problem

---
+++ app/src/main/java/com/example/mathematicalproblem/DialogFragmentCustom.java
@@ -0,0 +1,13 @@
+package com.example.mathematicalproblem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+
+public class DialogFragmentCustom {
+    public void setArguments(Bundle args) {
+    }
+
+    public void show(FragmentManager supportFragmentManager, String display_result) {
+    }
+}
